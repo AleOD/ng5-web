561e2080d71a9ed521c2e6857d75e815caeb071a
Ale Oliva <aleoliva9@hotmail.com>
2021-10-13 20:52:04 -0500

    change some values #5

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount: number = 4;
+  btntext: string = 'Add an item';
+
   constructor() { }
 
   ngOnInit(): void {
