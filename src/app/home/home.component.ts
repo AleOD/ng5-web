da81a80b9e1a0192255f62d5c1a9624b757d2c46
Ale Oliva <aleoliva9@hotmail.com>
2021-11-01 18:14:39 -0600

    correct errors

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,66 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 import { DataService } from '../data.service';
 import { GraphqlProductsService} from '../graphql.products.service';
 import { Subscription } from 'rxjs';
 import { GraphqlUsersService} from '../graphql.users.service';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-  animations: [
-    trigger('goals', [
-      transition('*=>*', [
-        query(':enter', style({opacity: 0}), {optional: true}),
-
-        query(':enter', stagger('300ms', [
-          animate('.6s ease-in', keyframes([
-            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
-            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
-            style({opacity: 1, transform: 'translateY(0)', offset: 1})
-          ]))
-        ]), {optional: true}),
-
-        query(':leave', stagger('300ms', [
-          animate('.6s ease-in', keyframes([
-            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
-            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
-            style({opacity: 0, transform: 'translateY(-75%)', offset: 1})
-          ]))
-        ]), {optional: true})
-      ])
-    ])
-  ]
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  itemCount: number = 4;
-  btnText: string = 'Add an item';
-  goalText: string = 'My first life goal';
+  itemCount: number = 0;
+  btnText: string = "add an item";
+  goalText: string = ""
   user: string = ""
   pass: string = ""
   token: string = ""
-  goals :any;
-  
-  loading: boolean;
+
+  goals = [];
+
+  loading: boolean | undefined;
   private querySubscription: Subscription;
 
+
   constructor(private _data: DataService, 
-    private graphqlProductsService: GraphqlProductsService,
-    private graphqlUsersService : GraphqlUsersService) { }
+              private graphqlProductsService: GraphqlProductsService,
+              private graphqlUsersService : GraphqlUsersService) { }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.itemCount = this.goals.length;
-
-
-/*    this._data.goal.subscribe(
-       res => 
-            this.goals = res
-    );
-    this._data.changeGoal(this.goals);
-*/
-
+    
     this.querySubscription = this.graphqlProductsService.links("-")
       .valueChanges
       .subscribe(({ data, loading }) => {
@@ -68,17 +39,41 @@ export class HomeComponent implements OnInit {
         this.goals = JSON.parse(JSON.stringify(data)).links;
         console.log(JSON.stringify(this.goals))
       });
+
   }
 
-  addItem(){
-    this.goals.push(this.goalText);
-    this.goalText = '';
+  loginUser() {
+
+    alert(this.user + " - " + this.pass);
+    this.graphqlUsersService.tokenAuth(this.user, this.pass)
+    .subscribe(({ data }) => {
+       console.log('logged: ', JSON.stringify(data));
+       
+      this.token =  JSON.parse(JSON.stringify(data)).tokenAuth.token;
+    }, (error) => {
+       console.log('there was an error sending the query', error);
+    });
+  
+  }  
+
+  addItem() {
+    var mytoken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImFkc29mdCIsImV4cCI6MTYzNDc5NTgwMiwib3JpZ0lhdCI6MTYzNDc5NTUwMn0._6tOsFeS7Li59toIpxyD1rDNi_HXnjvcDPBjoKXNcUk";
+    alert(this.goalText);
+
+    this.graphqlProductsService.createLink(mytoken, "https://www.github.com", this.goalText)
+    .subscribe(({ data }) => {
+       console.log('link created :  ', data);
+    }, (error) => {
+       console.log('there was an error sending the query', error);
+    });
+
+    this.goalText = "";
     this.itemCount = this.goals.length;
     this._data.changeGoal(this.goals);
   }
 
-  removeItem(i: number){
-    this.goals.splice(i, 1);
+  removeItem(i:any) {
+    this.goals.splice(i,1);
     this._data.changeGoal(this.goals);
   }
 }
